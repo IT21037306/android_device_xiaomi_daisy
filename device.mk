17a4076b89423f2bb6907106d7e59bcd0327ebe9
Ahamed Fahmi <it21037306@my.sliit.lk>
2023-02-03 12:01:14 +0530

    daisy : Switch BtAudio to AIDL

--- device.mk
+++ device.mk
@@ -149,9 +149,8 @@ PRODUCT_PACKAGES += \
 # A2DP
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0.vendor
 
 # Audio configuration
