673f3d0291e10491dac940e2f34e6101c174e053
Ahamed Fahmi <it21037306@my.sliit.lk>
2023-07-23 11:13:23 +0530

    daisy: Removed BT duplicate entries

--- device.mk
+++ device.mk
@@ -134,7 +134,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudiopreprocessing \
@@ -150,7 +149,7 @@ PRODUCT_PACKAGES += \
 # A2DP
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl \
+    #android.hardware.bluetooth.audio@2.1-impl \#
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0.vendor
